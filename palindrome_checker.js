9fed8e89670c42a46e2c70f46beeff131bdc8df9
David Umoru <umorudavido@gmail.com>
2023-07-25 12:07:58 +0100

    Initial Commit.

--- palindrome_checker.js
+++ palindrome_checker.js
@@ -2,7 +2,7 @@ function palindrome(string) {
   // Remove non-alphanumeric characters and convert to lowercase
   const cleanedString = string.replace(/[\W_]/g, "").toLowerCase();
 
-  // Compare cleaned string with reverse
+  // Create reversed version of the cleaned string
   const reversedString = cleanedString.split("").reverse().join("");
 
   // Check if the cleaned string equals its reverse
